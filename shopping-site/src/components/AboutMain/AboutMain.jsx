0692c3d786285748968cb73decd2d0c0125e17cf
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-08-27 19:42:15 +0800

    Created an AboutMain component to contain the main section for my about page

---
+++ shopping-site/src/components/AboutMain/AboutMain.jsx
@@ -0,0 +1,27 @@
+export default function AboutMain(){
+    return (
+        <main>
+            <div>
+                <h1>A Shop That You Can Trust</h1>
+                <p>
+                    With our patented super secure, not unbreakable, and high quality verification system, 
+                    you will never have to worry about your items not getting shipped to you. We are the world's
+                    #1 shopping site for a great reason. Additionally, our shop items are of the utmost quality. We
+                    sell and ship the most top notch clothing that anyone can buy for an incredibly cheap price
+                </p>
+            </div>
+            <div>
+                <h1>Who We Are</h1>
+                <p>
+                    We are a team of the most highly skilled developers and businuessmen all catered to making sure you 
+                    have an amazing time shopping using Shopple. Your experience is our top priority and we want to make sure 
+                    that you don't just have to buy clothes, we want you to want to buy clothes. Our mission is to make sure you
+                    have an enjoyable time shopping so that it no longer feels like a chore.
+                </p>
+            </div>
+            
+
+            
+        </main>
+    )
+}
