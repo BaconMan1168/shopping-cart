54f1dd16380ab7b51f33afff5fb04141a949f8d4
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-08-27 08:59:26 +0800

    Just to push to main

--- shopping-site/src/components/HeaderNav/HeaderNav.jsx
+++ shopping-site/src/components/HeaderNav/HeaderNav.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
 import cartIcon from "./icons8-shopping-cart-30.png"
 
 export default function HeaderNav(){
+    const [currPage, setCurrPage] = useState("Home");
 
 
     return (
         <header>
             <h1>Shopple</h1>
             <nav>
-                <a>Home</a>
-                <a>About</a>
-                <a>Shop</a>
+                <a className={currPage === "Home" ? "on-focus" : ""}>Home</a>
+                <a className={currPage === "About" ? "on-focus" : ""}>About</a>
+                <a className={currPage === "Shop" ? "on-focus" : ""}>Shop</a>
             </nav>
             <img src={cartIcon}></img>
         </header>
