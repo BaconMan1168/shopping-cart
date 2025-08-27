8fdc49045e1c96a30fc72bcaeba917bc366afc99
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-08-27 20:39:23 +0800

    CartMain component returns a main element and inside is an array of CartItem components created using cartProducts

--- shopping-site/src/components/CartMain/CartMain.jsx
+++ shopping-site/src/components/CartMain/CartMain.jsx
@@ -1,3 +1,9 @@
+import CartItem from "./CartItem.jsx"
+
 export default function CartMain({cartProducts, handleCartRemove}){
-    
+    return (
+        <main>
+            {cartProducts.map(product => <CartItem cartProduct={product} handleCartRemove={handleCartRemove} />)}
+        </main>
+    )
 }
