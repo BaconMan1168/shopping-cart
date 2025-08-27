b577f62cae9e689ededa2d26701522f52ff5ae7b
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-08-27 20:24:06 +0800

    ShopItem component contains all the jsx with necessary item information along with an input field to choose the quantity

---
+++ shopping-site/src/components/ShopItem/ShopItem.jsx
@@ -0,0 +1,19 @@
+export default function ShopItem({handleCartAdd, product}){
+    return (
+        <div>
+            <h1>{product.title}</h1>
+            <h2>{product.description}</h2>
+
+            <h3>{product.category}</h3>
+            <img src={product.image} alt="" />
+            <p>{product.price}</p>
+
+            <div>
+                <label htmlFor="quantity">Qty:</label>
+                <input type="number" id="quantity" name="quantity" />
+                <button onClick={handleCartAdd}>Add To Cart</button>
+            </div>
+            
+        </div>
+    )
+}
