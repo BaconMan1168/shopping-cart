69c2e9aecd3483bf057a9477b6c2e824064e0c0a
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-08-27 20:10:03 +0800

    ShopMain component contains a state variable to hold all the product objects and a useEffect hook to fetch them on mount

--- shopping-site/src/components/ShopMain/ShopMain.jsx
+++ shopping-site/src/components/ShopMain/ShopMain.jsx
@@ -1,6 +1,11 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function ShopMain({handleCartAdd}){
+    const [products, setProducts] = useState([]);
 
-
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(data =>  setProducts(data));
+    }, [])
 }
