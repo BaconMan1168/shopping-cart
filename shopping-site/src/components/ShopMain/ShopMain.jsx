235ad7954a0e0a463d812ecad5df7b8a974b454d
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-08-27 20:13:21 +0800

    Added error handling for both full network errors and errors for fetching incorrect data

--- shopping-site/src/components/ShopMain/ShopMain.jsx
+++ shopping-site/src/components/ShopMain/ShopMain.jsx
@@ -2,10 +2,20 @@ import { useEffect, useState } from "react"
 
 export default function ShopMain({handleCartAdd}){
     const [products, setProducts] = useState([]);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
-        .then(response => response.json())
-        .then(data =>  setProducts(data));
+        .then(response => {
+            if (response.status >= 400){
+                throw new Error("server error");
+            }
+
+            return response.json()
+        })
+        .then(data =>  setProducts(data))
+        .catch(error => setError(error));
     }, [])
+
+    if (error) return <h1>A network error has occured.</h1>
 }
