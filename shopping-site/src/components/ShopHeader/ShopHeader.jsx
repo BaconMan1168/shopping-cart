f3c548e30bea744f6a935c9730416b1a50485359
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-08-30 13:17:55 +0800

    Header links now follow the green/white color pallete of the website

--- shopping-site/src/components/ShopHeader/ShopHeader.jsx
+++ shopping-site/src/components/ShopHeader/ShopHeader.jsx
@@ -14,12 +14,12 @@ const ShopHeader = () => {
         <header className={styles.header}>
             <h1>Shopple</h1>
             <nav className={styles.navHeader}>
-                <Link to="/" className={currPage === "Home" ? "on-focus" : ""} onClick={handleClick}>Home</Link>
-                <Link to="/about" className={currPage === "About" ? "on-focus" : ""} onClick={handleClick}>About</Link>
-                <Link to="/shop" className={currPage === "Shop" ? "on-focus" : ""} onClick={handleClick}>Shop</Link>
+                <Link to="/" className={currPage === "Home" ? `on-focus ${styles.navLinks}` : `${styles.navLinks}`} onClick={handleClick}>Home</Link>
+                <Link to="/about" className={currPage === "About" ? `on-focus ${styles.navLinks}` : `${styles.navLinks}`} onClick={handleClick}>About</Link>
+                <Link to="/shop" className={currPage === "Shop" ? `on-focus ${styles.navLinks}` : `${styles.navLinks}`} onClick={handleClick}>Shop</Link>
                 
             </nav>
-            <Link to="/cart" className={currPage === "Cart" ? "on-focus cartImg" : `${styles.cartImg}`} onClick={handleClick}>
+            <Link to="/cart" className={currPage === "Cart" ? `on-focus ${styles.cartImg}` : `${styles.cartImg}`} onClick={handleClick}>
                 <img className={styles.cartImg} src={cartIcon}></img>
             </Link>
             
