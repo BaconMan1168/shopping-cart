c89e04cfa1e67d2c4f4ec2bff30228d83946ef38
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-08-31 19:46:45 +0800

    Added proper styling for home page main section (HomeMain)

--- shopping-site/src/components/HomeMain/HomeMain.jsx
+++ shopping-site/src/components/HomeMain/HomeMain.jsx
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom"
+import styles from "./HomeMain.module.css"
 
 export default function HomeMain(){
     return (
-        <main>
-            <h3>Welcome to Shopple</h3>
-            <h1>Elevate your shopping experience with Shopple</h1>
-            <h2>Clothes as cheap as $5.00</h2>
+        <main className={styles.homeMain}>
+            <div className={styles.leftElements}>
+                <h3>Welcome to Shopple</h3>
+                <h1 className={styles.homeHeader}>Elevate your shopping experience with Shopple</h1>
+                <h2>Clothes as cheap as $5.00</h2>
+            </div>
+            
+            <div className={styles.rightElements}>
+                <Link to="Shop">
+                    <button className={styles.shopBtn}>SHOP NOW!</button>
+                </Link>
+                <h2>You won't regret your purchases!</h2>
+            </div>
 
-            <Link to="Shop">Shop Now!</Link>
+            
         </main>
     )
 }
